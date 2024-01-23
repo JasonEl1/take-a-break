7f5b9972f7b2d2d843506cd527a5cdda304d2d2b
JasonEl1 <1eliseijas@hdsb.ca>
2024-01-23 09:36:07 -0500

    use absolute path for workmode and cron script

--- reminder.py
+++ reminder.py
@@ -3,29 +3,37 @@
 import sys
 import tkinter as tk
 import subprocess
+import os
+
+fullpath = os.path.abspath(__file__)
+name_len = len(os.path.basename(__file__))
+temppath = fullpath[:-name_len]
+
+workmode_path = f"{temppath}/workmode.txt"
+addcron_path = f"{temppath}/addcron.sh"
 
 arg_count = len(sys.argv)
 if arg_count>1:
     if sys.argv[1] == "set":
         print("set work mode")
-        with open("workmode.txt","w") as file_write:
+        with open(workmode_path,"w") as file_write:
             file_write.write("set")
             file_write.close()
-            subprocess.call(['sh','./addcron.sh','set'])
+            subprocess.call(['sh',addcron_path,'set'])
     elif sys.argv[1] == "unset":
         print("unset work mode")
-        with open("workmode.txt","w") as file_write:
+        with open(workmode_path,"w") as file_write:
             file_write.write("unset")
             file_write.close()
-            subprocess.call(['sh','./addcron.sh','unset'])
+            subprocess.call(['sh',addcron_path,'unset'])
     elif sys.argv[1] == "get":
         print("current work mode: ")
-        with open("workmode.txt","r") as file_read:
+        with open(workmode_path,"r") as file_read:
             print(file_read.readlines()[0])
             file_read.close()
 else:
     mode = ""
-    with open("workmode.txt","r") as file_read:
+    with open(workmode_path,"r") as file_read:
         mode = file_read.readlines()[0]
         if mode == "set":
             window = tk.Tk()
