b650ecb3debb183d0219d587adafe295c5d36f1e
JasonEl1 <1eliseijas@hdsb.ca>
2024-01-24 23:30:09 -0500

    add next and update commands

--- reminder.py
+++ reminder.py
@@ -4,6 +4,9 @@ import sys
 import tkinter as tk
 import subprocess
 import os
+import datetime
+
+version = "v0.2.0"
 
 fullpath = os.path.abspath(__file__)
 name_len = len(os.path.basename(__file__))
@@ -11,6 +14,30 @@ temppath = fullpath[:-name_len]
 
 workmode_path = f"{temppath}/workmode.txt"
 addcron_path = f"{temppath}/addcron.sh"
+
+def check_next(): #needs to return time to next in minutes
+    current_crontab = subprocess.check_output(['crontab','-l'])
+    current_crontab = current_crontab.decode('utf-8')
+    correct_entry = ""
+    for line in current_crontab.splitlines():
+        if line.endswith("reminder.py"):
+            correct_entry = line
+            break
+    current_time = datetime.datetime.now()
+    current_time = current_time.minute
+    correct_entry = correct_entry.split()[0]
+    first_time = correct_entry.split(',')[0]
+    first_time = int(first_time)
+    second_time = correct_entry.split(',')[1]
+    second_time = int(second_time)
+    if current_time > first_time and current_time > second_time:
+        return first_time - current_time
+    elif current_time >= first_time and current_time < second_time:
+        return second_time - current_time
+    elif current_time <= first_time and current_time < second_time:
+        return first_time - current_time
+    else:
+        print("Logical error...")
 
 arg_count = len(sys.argv)
 if arg_count>1:
@@ -27,10 +54,26 @@ if arg_count>1:
             file_write.close()
             subprocess.call(['sh',addcron_path,'unset',temppath[:-1]])
     elif sys.argv[1] == "get":
-        print("current work mode: ")
         with open(workmode_path,"r") as file_read:
-            print(file_read.readlines()[0])
+            print(f"current work mode: {file_read.readlines()[0]}")
             file_read.close()
+    elif sys.argv[1] == "next":
+        try:
+            next = check_next()
+            print(f"Next reminder is in {next} minutes.")
+        except:
+            print("Please enable work mode to check next reminder")
+    elif sys.argv[1] == "update":
+        mode = ""
+        with open(workmode_path,"r") as file_read:
+            mode = file_read.readlines()[0]
+        if mode == "set":
+            subprocess.call(['sh',addcron_path, 'unset',temppath[:-1]])
+            subprocess.call(['sh',addcron_path, 'set',temppath[:-1]])
+            next = check_next()
+            print(f"Updated. Next reminder is in {next} minutes.")
+        else:
+            print("Please enable work mode to update")
     else:
         print("Unknown command, please try again")
         exit()
@@ -39,11 +82,9 @@ else:
     with open(workmode_path,"r") as file_read:
         mode = file_read.readlines()[0]
         if mode == "set":
-            window = tk.Tk()
-            window.geometry("400x400")
-            window.attributes("-topmost", True)
-            window.eval('tk::PlaceWindow . center')
-            window.title("TAKE A BREAK!")
-            reminder = tk.Label(window,text="Reminder")
-            reminder.pack()
-            window.mainloop()
+            root = tk.Tk()
+            root.geometry("400x0")
+            root.attributes("-topmost", True)
+            root.eval('tk::PlaceWindow . center')
+            root.title("TAKE A BREAK!")
+            root.mainloop()
