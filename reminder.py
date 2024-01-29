944508b592497727f7eb8e97c2a7ea46e9f6180d
JasonEl1 <1eliseijas@hdsb.ca>
2024-01-28 23:47:38 -0500

    moved write and read to functions and fixed logical error

--- reminder.py
+++ reminder.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# v0.3.0
+# v0.4.0
 
 import sys
 import tkinter as tk
@@ -15,6 +15,19 @@ workmode_path = f"{temppath}/workmode.txt"
 addcron_path = f"{temppath}/addcron.sh"
 sound_path = f"{temppath}/sound.wav"
 
+def write_work_mode(mode):
+    with open(workmode_path,"w") as file_write:
+        file_write.write(mode)
+        file_write.close()
+        subprocess.call(['sh',addcron_path,mode,temppath[:-1]])
+    print(f"{mode} work mode")
+
+def read_work_mode():
+    with open(workmode_path,"r") as file_read:
+        mode = file_read.readlines()[0]
+        return mode
+        file_read.close()
+
 def check_next():
     current_crontab = subprocess.check_output(['crontab','-l'])
     current_crontab = current_crontab.decode('utf-8')
@@ -26,11 +39,10 @@ def check_next():
     current_time = datetime.datetime.now()
     current_time = current_time.minute
     correct_entry = correct_entry.split()[0]
-    first_time = correct_entry.split(',')[0]
-    first_time = int(first_time)
-    second_time = correct_entry.split(',')[1]
-    second_time = int(second_time)
-    if current_time > first_time and current_time > second_time:
+    first_time = int(correct_entry.split(',')[0])
+    second_time = int(correct_entry.split(',')[1])
+
+    if current_time > first_time and current_time >= second_time:
         return 60 - current_time + first_time
     elif current_time >= first_time and current_time < second_time:
         return second_time - current_time
@@ -42,21 +54,11 @@ def check_next():
 arg_count = len(sys.argv)
 if arg_count>1:
     if sys.argv[1] == "set":
-        print("set work mode")
-        with open(workmode_path,"w") as file_write:
-            file_write.write("set")
-            file_write.close()
-            subprocess.call(['sh',addcron_path,'set',temppath[:-1]])
+        write_work_mode("set")
     elif sys.argv[1] == "unset":
-        print("unset work mode")
-        with open(workmode_path,"w") as file_write:
-            file_write.write("unset")
-            file_write.close()
-            subprocess.call(['sh',addcron_path,'unset',temppath[:-1]])
+        write_work_mode("unset")
     elif sys.argv[1] == "get":
-        with open(workmode_path,"r") as file_read:
-            print(f"current work mode: {file_read.readlines()[0]}")
-            file_read.close()
+        print(f"Current mode is {read_work_mode()}")
     elif sys.argv[1] == "next":
         try:
             next = check_next()
@@ -64,10 +66,7 @@ if arg_count>1:
         except:
             print("Please enable work mode to check next reminder")
     elif sys.argv[1] == "update":
-        mode = ""
-        with open(workmode_path,"r") as file_read:
-            mode = file_read.readlines()[0]
-        if mode == "set":
+        if read_work_mode() == "set":
             subprocess.call(['sh',addcron_path, 'unset',temppath[:-1]])
             subprocess.call(['sh',addcron_path, 'set',temppath[:-1]])
             next = check_next()
@@ -78,24 +77,21 @@ if arg_count>1:
         print("Unknown command, please try again")
         exit()
 else:
-    mode = ""
-    with open(workmode_path,"r") as file_read:
-        mode = file_read.readlines()[0]
-        if mode == "set":
-            os_type = subprocess.check_output(['uname'])
-            os_type = os_type.decode('utf-8').strip("\n")
-            process = None
-            if os_type == "Darwin":
-                process = subprocess.Popen(['afplay',sound_path])
-            elif os_type == "Linux":
-                process = subprocess.Popen(['aplay',sound_path])
-            else:
-                print("Your OS is not yet supported.")
-            root = tk.Tk()
-            root.geometry("400x100")
-            root.attributes("-topmost", True)
-            root.eval('tk::PlaceWindow . center')
-            root.title("TAKE A BREAK!")
-            root.mainloop()
-            if process:
-                process.terminate()
+    if read_work_mode() == "set":
+        os_type = subprocess.check_output(['uname'])
+        os_type = os_type.decode('utf-8').strip("\n")
+        process = None
+        if os_type == "Darwin":
+            process = subprocess.Popen(['afplay',sound_path])
+        elif os_type == "Linux":
+            process = subprocess.Popen(['aplay',sound_path])
+        else:
+            print("Your OS is not yet supported.")
+        root = tk.Tk()
+        root.geometry("400x100")
+        root.attributes("-topmost", True)
+        root.eval('tk::PlaceWindow . center')
+        root.title("TAKE A BREAK!")
+        root.mainloop()
+        if process:
+            process.terminate()
