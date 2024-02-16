b6667fb460db4ef6001152d067aaaa71964d24c6
JasonEl1 <1eliseijas@hdsb.ca>
2024-02-16 12:17:06 -0500

    fix bug for setting work mode at x:00

--- scripts/addcron.sh
+++ scripts/addcron.sh
@@ -6,6 +6,11 @@ mins=$(date +%M)
 mins=$((mins-1))
 opp_mins=0
 
+if [ $mins == -1 ]; then
+    mins=59
+    opp_mins=29
+fi
+
 if [ $mins -lt 30 ]; then
     opp_mins=$(($mins+30))
 else
@@ -22,7 +27,6 @@ else
     first=$opp_mins
     second=$mins
 fi
-
 
 if [ $1 == "set" ]; then
     crontab -l | { cat; echo "$first,$second * * * * cd $2 && source venv/bin/activate && /usr/bin/python3 reminder.py && source scripts/deactivate"; } | crontab -
