a93f2ecd8c3a7c5e588e6538363c59d296a3e3d9
JasonEl1 <1eliseijas@hdsb.ca>
2025-10-30 03:23:04 -0400

    Update launcher paths and argument logic

--- launcher.go
+++ launcher.go
@@ -3,14 +3,28 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"os/user"
+	"path/filepath"
 )
 
 func main() {
-	if len(os.Args) == 3 {
-		exec.Command("python3", "reminder.py", os.Args[1], "-t", os.Args[2])
+	var cmd *exec.Cmd
+	usr, _ := user.Current()
+
+	if len(os.Args) == 4 {
+		cmd = exec.Command("python3", filepath.Join(usr.HomeDir, ".local/share/take-a-break/reminder.py"), os.Args[1], "-t", os.Args[3])
 	} else {
-		exec.Command("python3", "reminder.py", os.Args[1]) //check
+		cmd = exec.Command("python3", filepath.Join(usr.HomeDir, ".local/share/take-a-break/reminder.py"), os.Args[1])
 	}
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		log.Fatalf("%v\n%s", err, output)
+	}
+
+	fmt.Printf("%s", output)
 }
